b358c0222e840a813aa8ddbf18a6c29d5d007a88
Claudia M Paz <claudia.paz@gds.ey.com>
2024-08-14 01:07:26 -0300

    Empty Project

---
+++ UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace playwright_bootcamp_csharp;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
